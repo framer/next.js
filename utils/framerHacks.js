bb891f612b7cbe8986d23ce0fb4816fd0256bc01
Hunter Caron <huntercaron@me.com>
2021-08-03 20:06:33 +0200

    Basic demo

--- utils/framerHacks.js
+++ utils/framerHacks.js
@@ -1,3 +1,6 @@
 // Necessary for Framer library to work in Node
 globalThis.navigator ??= { userAgent: "" }
 globalThis.window ??= globalThis
+
+window.Framer = require("framer")
+window.React = require("react")
--- webpack/EsmUrlPlugin.cjs
+++ webpack/EsmUrlPlugin.cjs
@@ -1,9 +1,14 @@
-"use strict";
+"use strict"
 
-const { URL } = require("url");
-const webpack = require("webpack");
+const { URL } = require("url")
+const webpack = require("webpack")
 
 /** @typedef {webpack.Compiler} Compiler */
+
+// We'll cache redirected URLs to ensure relative paths are resolved correctly.
+// TODO: Make sure Webpack gives us the final URL, not the initial one.
+/** @type {Record<string, URL>} */
+const redirectCache = {}
 
 class EsmUrlPlugin {
     /**
@@ -17,82 +22,141 @@ class EsmUrlPlugin {
             (compilation, { normalModuleFactory }) => {
                 normalModuleFactory.hooks.resolveForScheme
                     .for("https")
-                    .tap("EsmUrlPlugin", resourceData => {
-                        const url = new URL(resourceData.resource);
-                        resourceData.path = url.origin + url.pathname;
-                        resourceData.query = url.search;
-                        resourceData.fragment = url.hash;
-                        return /** @type {true} */ (true);
-                    });
+                    .tapAsync(
+                        "EsmUrlPlugin",
+                        (resourceData, resolveContext, callback) => {
+                            const url = new URL(resourceData.resource)
+
+                            const { https } = require("follow-redirects")
+                            https.get(url, { maxRedirects: 10 }, (res) => {
+                                if (res.statusCode !== 200) {
+                                    res.destroy()
+                                    return callback(
+                                        new Error(
+                                            `https request status code = ${res.statusCode}`
+                                        )
+                                    )
+                                }
+
+                                const finalUrl = new URL(res.responseUrl)
+                                resourceData.path =
+                                    finalUrl.origin + finalUrl.pathname
+                                resourceData.query = finalUrl.search
+                                resourceData.fragment = finalUrl.hash
+                                if (finalUrl.href !== url.href) {
+                                    redirectCache[url.href] = finalUrl
+                                }
+
+                                return callback()
+                            })
+
+                            // return /** @type {true} */ (true);
+                        }
+                    )
                 webpack.NormalModule.getCompilationHooks(compilation)
                     .readResourceForScheme.for("https")
                     .tapAsync("EsmUrlPlugin", (resource, module, callback) => {
-                        const { https } = require("follow-redirects");
-                        return https.get(new URL(resource), { maxRedirects: 10 }, res => {
-                            if (res.statusCode !== 200) {
-                                res.destroy();
-                                return callback(
-                                    new Error(`https request status code = ${res.statusCode}`)
-                                );
-                            }
-
-                            const bufferArr = [];
-
-                            res.on("data", chunk => {
-                                bufferArr.push(chunk);
-                            });
-
-                            res.on("end", () => {
-                                if (!res.complete) {
-                                    return callback(new Error("https request was terminated"));
+                        const { https } = require("follow-redirects")
+                        return https.get(
+                            new URL(resource),
+                            { maxRedirects: 10 },
+                            (res) => {
+                                if (res.statusCode !== 200) {
+                                    res.destroy()
+                                    return callback(
+                                        new Error(
+                                            `https request status code = ${res.statusCode}`
+                                        )
+                                    )
                                 }
 
-                                callback(null, Buffer.concat(bufferArr));
-                            });
-                        });
-                    });
+                                const bufferArr = []
+
+                                res.on("data", (chunk) => {
+                                    bufferArr.push(chunk)
+                                })
+
+                                res.on("end", () => {
+                                    if (!res.complete) {
+                                        return callback(
+                                            new Error(
+                                                "https request was terminated"
+                                            )
+                                        )
+                                    }
+
+                                    callback(null, Buffer.concat(bufferArr))
+                                })
+                            }
+                        )
+                    })
             }
-        );
+        )
 
         // Register our custom resolver for relative paths, URLs and bare module specifiers.
         compiler.resolverFactory.hooks.resolver
-            .for('normal')
-            .tap('EsmUrlPlugin', (resolver) => {
+            .for("normal")
+            .tap("EsmUrlPlugin", (resolver) => {
                 // If we successfully resolve a URL we jump straight to the last step of the resolution pipeline:
                 // https://github.com/webpack/enhanced-resolve/blob/60d79f3c93304ce5ecbbe0127aa583d4a73bf1a1/lib/ResolverFactory.js#L279
-                const target = resolver.ensureHook("resolved");
+                const target = resolver.ensureHook("resolved")
 
-                resolver.hooks.resolve.tapAsync('EsmUrlPlugin', (request, resolveContext, callback) => {
-                    const baseUrl = parseUrl(request.context.issuer);
+                resolver.hooks.resolve.tapAsync(
+                    "EsmUrlPlugin",
+                    (request, resolveContext, callback) => {
+                        let baseUrl = parseUrl(request.context.issuer)
+                        // If the issuer module is not a URL module, skip.
+                        if (!baseUrl) return callback()
 
-                    // If the issuer module is not a URL module, skip.
-                    if (!baseUrl) return callback();
+                        baseUrl = redirectCache[baseUrl.href] || baseUrl
 
-                    if (needsRelativeResolution(request.request)) {
-                        request.path = new URL(request.request, baseUrl).href
+                        if (needsRelativeResolution(request.request)) {
+                            request.path = new URL(
+                                request.request,
+                                baseUrl
+                            ).href
 
-                        return resolver.doResolve(target, request, null, resolveContext, callback);
+                            return resolver.doResolve(
+                                target,
+                                request,
+                                null,
+                                resolveContext,
+                                callback
+                            )
+                        }
+
+                        if (!parseUrl(request.request)) {
+                            // We are dealing with a bare module specifier.
+                            // Resolve it starting from the roots of the project.
+                            request.path = require.resolve(request.request, {
+                                paths: Array.from(resolver.options.roots),
+                            })
+
+                            return resolver.doResolve(
+                                target,
+                                request,
+                                null,
+                                resolveContext,
+                                callback
+                            )
+                        }
+
+                        return callback()
                     }
-
-                    if (!parseUrl(request.request)) {
-                        // We are dealing with a bare module specifier.
-                        // Resolve it starting from the roots of the project.
-                        request.path = require.resolve(request.request, { paths: Array.from(resolver.options.roots) })
-
-                        return resolver.doResolve(target, request, null, resolveContext, callback);
-                    }
-
-                    return callback();
-                });
+                )
             })
     }
 }
 
-module.exports = EsmUrlPlugin;
+module.exports = EsmUrlPlugin
 
 function needsRelativeResolution(path) {
     // Relative paths, absolute paths and protocol-relative URLs need resolution against the issuer URL.
-    return path.startsWith('.') || path.startsWith('/') || path.startsWith("assets/") // FIXME: this is a Framer hack
+    return (
+        path.startsWith(".") ||
+        path.startsWith("/") ||
+        path.startsWith("assets/")
+    ) // FIXME: this is a Framer hack
 }
 
 function parseUrl(maybeURL) {
