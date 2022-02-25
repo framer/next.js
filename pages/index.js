79f9322daaf0519cc7c4e7ec3db5daf082cb25c0
New.kub <71245711+newkub@users.noreply.github.com>
2022-02-25 18:51:41 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import { Tip } from "../components/Tip"
 import { Container } from "../components/Container"
+import Vocabcard from "https://framer.com/m/vocabcard-DTEj.js@JCLkYdKFeDtqml4Zqyjw"
 
 // A Smart Component from Framer
 import Toggle from "https://framer.com/m/Toggle-B5iT.js@X94mtocAj5XithxLESme"
@@ -8,6 +9,12 @@ import Toggle from "https://framer.com/m/Toggle-B5iT.js@X94mtocAj5XithxLESme"
 export default function Home() {
     return (
         <>
+
+            <Vocabcard
+                variant="card"
+            />
+
+            import Vocabcard from "https://framer.com/m/vocabcard-DTEj.js@JCLkYdKFeDtqml4Zqyjw"
             <Head>
                 <title>Framer</title>
                 <meta name="description" content="Modules" />
